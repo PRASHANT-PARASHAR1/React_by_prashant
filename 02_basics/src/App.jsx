de1bb4d46883ee88f4acf7e4c00251a8daf90361
Prashant Parashar <prashant112006@gmail.com>
2026-08-12 14:25:03 +0530

    update app.jsx

--- 02_basics/src/App.jsx
+++ 02_basics/src/App.jsx
@@ -5,18 +5,16 @@ function App() {
 
   return (
     <>
-      
         <h1>Resume</h1>
         <h3>.Prashant Parashar from Lalpur Tappal Aligarh</h3>
         <h2>Education</h2>
-        <h3>.schooling from up board and diploma in electronics enginnearing and currently in 
-          pursuing btech from electronics & communication enginearing
+        <h3>.schooling from up board and diploma in electronics enginnearing and currently  
+          pursuing in btech from electronics & communication enginearing
         </h3>
         <h2>Hobby</h2>
-        <h3>Playing the cricket match and watch also</h3>
+        <h3>Playing the cricket and watch also</h3>
            
-               
-                
+                  
     </>
   )
 }
