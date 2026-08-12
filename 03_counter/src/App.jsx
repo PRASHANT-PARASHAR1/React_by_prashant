cfbbc6874acdd5da0bd068bc909c4b5be246aa95
Prashant Parashar <prashant112006@gmail.com>
2026-08-12 15:56:09 +0530

    counter

--- 03_counter/src/App.jsx
+++ 03_counter/src/App.jsx
@@ -17,7 +17,7 @@ const RemoveVal = () =>{
   return (
     <>
       
-     <h1>chai aur react</h1>
+     <h1>counter by prashant parashar</h1>
      <h2>Counter value:{counter}</h2>
      <button
      onClick={addValue}
